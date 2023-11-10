c974568a62151e0f66245ffff5e65a7e0978f777
IvoTab11 <142843560+IvoTab11@users.noreply.github.com>
2023-11-10 01:40:49 -0300

    Punto 10 realizado: el jugador puede disparar con la barra espaciadora

--- practica01/Assets/Scripts/PlayerMovement.cs
+++ practica01/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float speed=2f;
+    [SerializeField]
+    private GameObject ballPlayer;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Input.GetAxis("Horizontal")*speed*Time.deltaTime,0,0);
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Instantiate(ballPlayer,transform.position,transform.rotation);
+        }
     }
 }
