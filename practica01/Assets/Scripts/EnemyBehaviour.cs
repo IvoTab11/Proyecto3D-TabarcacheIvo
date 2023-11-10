dd94d1e54650d1623b36e7c86239077dde89ece5
IvoTab11 <142843560+IvoTab11@users.noreply.github.com>
2023-11-09 22:43:32 -0300

    Punto 7 realizado: se creó el cubo "Enemigo02" y se modificó el script "EnemyBehaviour" para su movimiento y para instanciar "BalaEnemigo".

--- practica01/Assets/Scripts/EnemyBehaviour.cs
+++ practica01/Assets/Scripts/EnemyBehaviour.cs
@@ -10,17 +10,54 @@ public class EnemyBehaviour : MonoBehaviour
     private float speedX=2f;
     [SerializeField]
     private float speedZ=2f;
+    [SerializeField]
+    private int enemyType;
+    [SerializeField]
+    private float speedXEnemy02=2f;
+    private float randomTime=0;
     private void Start(){
-        InvokeRepeating("ShootBall",0,2f);
+        switch(enemyType){
+            case 1:
+            {
+                InvokeRepeating("ShootBall",0,2f);
+                break;
+            }
+            case 2: {
+                Invoke("ShootBall",randomTime);
+                break;
+            }
+        }
+   
     }
 
     void Update()
     {
-        transform.Translate(speedX*Time.deltaTime,0,speedZ*Time.deltaTime);
+        moveEnemy();
     }
 
     public void ShootBall(){
         Instantiate(enemyBall,transform.position,transform.rotation);
+        if(enemyType==2){
+            randomTime = Random.Range(1,3);
+            Invoke("ShootBall",randomTime);
+        }
+    }
 
+    public void moveEnemy(){
+        switch(enemyType){
+            case 1:
+            {
+                transform.Translate(speedX*Time.deltaTime,0,speedZ*Time.deltaTime);
+                break;
+            }
+             case 2:
+            {
+                if(transform.position.x<-4.44f || transform.position.x>4.44f){
+                    speedXEnemy02*=-1;
+                }
+                transform.Translate(speedXEnemy02*Time.deltaTime,0,0);
+                break;
+            }
+        }
     }
 }
